ae60466bbc6a31cb82f039fc97ba898440d83e3b
Yusuf Umar Wali <payveraofficial@gmail.com>
2026-06-21 04:06:56 +0530

    Connected Firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'features/splash/screens/splash_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ZiaConnectApp());
 }
 
